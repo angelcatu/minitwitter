00a8b6ccde2406b74094164f03265c9257202cb5
Angel Manuel Elias <angel.manuel.elias.gt@gmail.com>
2021-06-10 14:07:29 -0600

    AuthInterceptor added

---
+++ app/src/main/java/com/tzikin/minitwitter/view/viewmodel/repository/retrofit/api/AuthInterceptor.java
@@ -0,0 +1,23 @@
+package com.tzikin.minitwitter.view.viewmodel.repository.retrofit.api;
+
+import com.tzikin.minitwitter.view.common.Constants;
+import com.tzikin.minitwitter.view.common.SharedPreferenceManager;
+
+import java.io.IOException;
+
+import okhttp3.Interceptor;
+import okhttp3.Request;
+import okhttp3.Response;
+
+public class AuthInterceptor implements Interceptor {
+    @Override
+    public Response intercept(Chain chain) throws IOException {
+        String token = SharedPreferenceManager.getSomeStringValue(Constants.PREF_TOKEN);
+        Request request = chain.request()
+                .newBuilder()
+                .addHeader("Authorization", "Bearer "+token)
+                .build();
+
+        return chain.proceed(request);
+    }
+}
