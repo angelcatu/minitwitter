e201828b6b075e889b2496f7c2fa46cf8823932c
Angel Manuel Elias <angel.manuel.elias.gt@gmail.com>
2021-06-25 12:44:40 -0600

    Call petition to New Tweet added

--- app/src/main/java/com/tzikin/minitwitter/view/common/Constants.java
+++ app/src/main/java/com/tzikin/minitwitter/view/common/Constants.java
@@ -2,6 +2,8 @@ package com.tzikin.minitwitter.view.common;
 
 public class Constants {
 
+    public static final String PHOTO_URL_SERVER = "https://www.minitwitter.com:3001/apiv1/uploads/photos";
+
     // Preferences
     public static final String PREF_TOKEN = "PREF_TOKEN";
     public static final String PREF_USERNAME = "PREF_USERNAME";
--- app/src/main/java/com/tzikin/minitwitter/view/ui/home/NewTweetDialogFragment.java
+++ app/src/main/java/com/tzikin/minitwitter/view/ui/home/NewTweetDialogFragment.java
@@ -1,16 +1,25 @@
 package com.tzikin.minitwitter.view.ui.home;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.arch.lifecycle.ViewModelProviders;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.DialogFragment;
+import androidx.lifecycle.ViewModelProvider;
 
+import com.bumptech.glide.Glide;
 import com.tzikin.minitwitter.R;
 import com.tzikin.minitwitter.databinding.NewTweetDialogBinding;
+import com.tzikin.minitwitter.view.common.Constants;
+import com.tzikin.minitwitter.view.common.SharedPreferenceManager;
 import com.tzikin.minitwitter.view.viewmodel.repository.repositories.TweetRepository;
 
 import org.jetbrains.annotations.NotNull;
@@ -18,6 +27,7 @@ import org.jetbrains.annotations.NotNull;
 public class NewTweetDialogFragment extends DialogFragment implements View.OnClickListener {
 
     private NewTweetDialogBinding binding;
+
 
     @Override
     public void onCreate(@Nullable @org.jetbrains.annotations.Nullable Bundle savedInstanceState) {
@@ -39,6 +49,11 @@ public class NewTweetDialogFragment extends DialogFragment implements View.OnCli
         super.onViewCreated(view, savedInstanceState);
 
         bindUIElements();
+
+        // Update profile image
+        String photoURL = SharedPreferenceManager.getSomeStringValue(Constants.PREF_PHOTO_URL);
+        if (!photoURL.isEmpty())
+            Glide.with(requireActivity()).load(Constants.PHOTO_URL_SERVER + photoURL).into(binding.imgProfileTweet);
     }
 
     private void bindUIElements() {
@@ -48,17 +63,45 @@ public class NewTweetDialogFragment extends DialogFragment implements View.OnCli
     @Override
     public void onClick(View v) {
         int id = v.getId();
-        switch (id){
+        String message = binding.edtTweetMessage.getText().toString();
+        switch (id) {
             case R.id.btnTweet:
-                String message =  binding.edtTweetMessage.getText().toString();
-                TweetRepository tweetRepository = new TweetRepository();
-                tweetRepository.postNewTweet(message);
-                tweetRepository.getPostNewTweet().observe(requireActivity(), response -> {
-                    if(response != null){
 
-                    }
-                });
+                if (!message.isEmpty()) {
+                    HomeViewModel tweetViewModel = new ViewModelProvider(requireActivity()).get(HomeViewModel.class);
+                    tweetViewModel.insertTweet(message);
+                } else {
+                    Toast.makeText(requireActivity(), "Escribir un mensaje para publicar", Toast.LENGTH_SHORT).show();
+                }
+
                 break;
+
+            case R.id.imgClose:
+                if (!message.isEmpty()) {
+                    showDialogConfirm();
+                } else {
+                    getDialog().dismiss();
+                }
+
+                break;
+
+
         }
+    }
+
+    private void showDialogConfirm() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(requireActivity());
+        builder.setMessage(requireActivity().getString(R.string.close_dialog_new_tweet));
+        builder.setTitle(requireActivity().getString(R.string.cancel_new_tweet));
+
+        builder.setPositiveButton(requireActivity().getString(R.string.eliminate), (dialog, which) -> {
+            dialog.dismiss();
+            getDialog().dismiss();
+        }).setNegativeButton(requireActivity().getString(R.string.cancel), (dialog, which) -> {
+            dialog.dismiss();
+            getDialog().dismiss();
+        }).show();
+
+        AlertDialog alertDialog = builder.create();
     }
 }
